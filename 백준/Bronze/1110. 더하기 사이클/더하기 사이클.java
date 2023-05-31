ca2d622c06d8a55fee5f8df82a0c51208145dc82
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-05-31 19:31:20 +0900

    [Bronze I] Title: 더하기 사이클, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,19 +1,22 @@
 import java.io.*;
 class Main{
-    public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		boolean flag = false;
-		int newN = N;
-		int cnt = 0;
-		while(!flag) {
-			int sum = newN/10 + newN%10;
-			newN = newN%10*10 + sum%10;
-			if(newN==N) {
-				flag = true;
-			}
-			cnt++;
-		}
-		System.out.println(cnt);
-    }
+    public static void main(String[] a) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        //N과 비교할 숫자
+        int newN = N;
+        boolean flag = false;
+        int cnt = 0;
+        while(!flag){
+            //새로운 수를 만드는 과정
+            int sum = newN/10 + newN%10;
+            //newN%10*10 = 26의 6을 앞으로 보내는 과정
+            //sum%10 = 새로운 수의 일의 자리를 뽑아내는 과정
+            newN = newN%10*10 + sum%10;
+            if(newN==N) flag = true;
+            cnt++;
+        }
+        System.out.print(cnt);
+    }    
 }
