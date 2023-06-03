3a106bac95816617a540d1ff8ebfc176c947749d
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-03 23:20:23 +0900

    [Silver V] Title: 소트인사이드, Time: 76 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -3,10 +3,8 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		char[] N = br.readLine().toCharArray();
 		Arrays.sort(N);
-		for(int i=N.length-1; i>-1; i--) bw.write(N[i]);
-		bw.flush(); bw.close();
+		System.out.println(new StringBuilder(new String(N)).reverse().toString());
 	}
 }
