413b3b5cd1b5488aa3b2dc2ada76a85e1957e141
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-03 23:05:04 +0900

    [Bronze I] Title: N번째 큰 수, Time: 116 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -7,11 +7,11 @@ class Main {
 		int T = Integer.parseInt(br.readLine());
 		StringTokenizer st;
 		while(T-->0) {
-			int[] arr = new int[10];
+			List<Integer> list = new ArrayList<>();
 			st = new StringTokenizer(br.readLine());
-			for(int i=0; i<10; i++) arr[i] = Integer.parseInt(st.nextToken());
-			Arrays.sort(arr);
-			bw.write(arr[7] + "\n");
+			for(int i=0; i<10; i++) list.add(Integer.parseInt(st.nextToken()));
+			Collections.sort(list);
+			bw.write(list.get(7) + "\n");
 		}
 		bw.flush(); bw.close();
 	}
