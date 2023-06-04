cdcee95d0b320d72c6b4b51188dd4f41651dda1c
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-04 23:57:59 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 412 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,24 +1,12 @@
 import java.io.*;
 class Main {
-	static int CNTDYNAMIC = 0;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int recursionCnt = 2 + (n-3)*(n-2)/2;
-		fibonacci(n);
-		System.out.println(fib(n) + " " + CNTDYNAMIC);
+		System.out.println(fib(n) + " " + (n-2));
 	}
 	private static int fib(int n) {
 		if(n==1 || n==2) return 1;
 		else return (fib(n-1) + fib(n-2));
-	}
-	private static int fibonacci(int n) {
-		int[] f = new int[n];
-		f[0] = 1; f[1] = 1;
-		for(int i=2; i<n; i++) {
-			CNTDYNAMIC++;
-			f[i] = f[i-1] + f[i-2];
-		}
-		return f[n-1];
 	}
 }
