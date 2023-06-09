b2a785aa53c45fe2a35a573d83d1ed1fff9a9132
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-10 04:37:23 +0900

    [Silver I] Title: 보석 도둑, Time: 96 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Silver/14232. 보석 도둑/보석 도둑.java
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -1,25 +1,24 @@
 import java.io.*;
-import java.util.*;
 class Main {
 	public static void main(String[] args) throws IOException {
-	Long K = Long.parseLong(new BufferedReader(new InputStreamReader(System.in)).readLine());
-		long divisor = 2, cnt = 0;
+		Long K = Long.parseLong(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		int cnt = 0;
 		StringBuilder sb = new StringBuilder();
-		while(true) {
-			if(K==1) break;
-            if (divisor>=1000000) {
-				sb.append(K);
-				cnt++;
-				break;
-			}	
-			if(K%divisor==0) {
-				cnt++;
-				K /= divisor;
-				sb.append(divisor).append(" ");
+		for(int i=2; i<=Math.sqrt(K); i++) {
+			for(;;) {
+				if(K%i==0) {
+					sb.append(i).append(" ");
+					K /= i;
+					cnt++;
+				}
+				else break;
 			}
-            else divisor++;
 		}
+		if(K!=1) {
+            sb.append(K);
+            cnt++;
+        }
 		System.out.println(cnt);
-		System.out.println(sb);
+		System.out.println(sb.toString());
 	}
 }
