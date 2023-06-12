84eef90f0acdd7b2698baac9ed7aabc7eff27b5b
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-12 19:27:41 +0900

    [Gold V] Title: 실질적 약수, Time: 416 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Gold/2247. 실질적 약수/실질적 약수.java
+++ 백준/Gold/2247. 실질적 약수/실질적 약수.java
@@ -1,13 +1,15 @@
 import java.io.*;
-class Main {
-	public static void main(String[] args) throws IOException {
-		System.out.println(csod(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine())));
-	}
-	private static long csod(int val) {
-		long result = 0;
-		for(int i=2; i<=val/2; i++) {
-			result += ((val/i)-1)*i;
-		}
-		return result%1000000;
-	}
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print(csod(N));
+    } 
+    static private long csod(int val){
+        long sum = 0;
+        for(int i=2; i<=val/2; i++){
+            sum += ((val/i)-1)*i;
+        }
+        return sum%1000000;
+    }
 }
