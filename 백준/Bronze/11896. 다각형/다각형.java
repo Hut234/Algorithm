e9d578626ee045a91f221acd202505efa3fde69e
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-17 04:49:20 +0900

    [Bronze I] Title: 다각형, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/11896. 다각형/다각형.java
+++ 백준/Bronze/11896. 다각형/다각형.java
@@ -7,8 +7,25 @@ class Main {
 		int b = Integer.parseInt(input[1]);
 		long sum = 0;
 		if(a%2!=0) a += 1;
+		if(b%2!=0) b -= 1;
 		if(a==2) a += 2;
-		for(int i=a;i<=b;i+=2) sum+=i;
+		if(a>b) {
+			System.out.println(0);
+			return;
+		}
+		if(b-a==2) {
+			System.out.println(a+b);
+			return;
+		}else if(b-a==0) {
+			System.out.println(a);
+			return;
+		}
+		int half = (b-a+1)/2 +1;
+		if(half%2==0) {
+			sum = (long)(a+b)*half/2;
+		} else {
+			sum = (long)(a+(b-2))*((half-1)/2) +b;
+		}
 		System.out.println(sum);
 	}
 }
