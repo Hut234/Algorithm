7e2c16d7a7774d5e35f5e15e8d6cd7184c427bc6
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-22 23:15:17 +0900

    [Bronze I] Title: 유니크, Time: 92 ms, Memory: 11880 KB -BaekjoonHub

---
+++ 백준/Bronze/5533. 유니크/유니크.java
@@ -0,0 +1,37 @@
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] num = new int[N][3];
+        for(int i=0; i<N; i++) {
+	    	String[] input = br.readLine().split(" ");
+            num[i][0] = Integer.parseInt(input[0]);
+            num[i][1] = Integer.parseInt(input[1]);
+            num[i][2] = Integer.parseInt(input[2]);
+        }  	
+        boolean flagA, flagB, flagC;
+        for(int i=0; i<N-1; i++) {
+            flagA = flagB = flagC = false;
+            for(int j=i+1; j<N; j++) {
+                if(num[i][0]==num[j][0]){
+                    flagA = true;
+                    num[j][0] = 0;
+                }
+                if(num[i][1]==num[j][1]){
+                    flagB = true;
+                    num[j][1] = 0;
+                }
+                if(num[i][2]==num[j][2]){
+                    flagC = true;
+                    num[j][2] = 0;
+                }
+            }
+            if(flagA) num[i][0] = 0;
+            if(flagB) num[i][1] = 0;
+            if(flagC) num[i][2] = 0;
+            System.out.println(num[i][0] + num[i][1] + num[i][2]);
+            if(i==N-2) System.out.println(num[i+1][0] + num[i+1][1] + num[i+1][2]);
+        }
+    }
+}
