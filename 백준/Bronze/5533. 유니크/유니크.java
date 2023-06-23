2b3f6a34ca60844b90fb9ff384ddc5f0ee939520
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-06-23 18:42:37 +0900

    [Bronze I] Title: 유니크, Time: 92 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Bronze/5533. 유니크/유니크.java
+++ 백준/Bronze/5533. 유니크/유니크.java
@@ -1,28 +1,28 @@
 import java.io.*;
-class Main {
-    public static void main(String[] args) throws IOException {
+class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] num = new int[N][3];
         for(int i=0; i<N; i++) {
-	    	String[] input = br.readLine().split(" ");
+            String[] input = br.readLine().split(" ");
             num[i][0] = Integer.parseInt(input[0]);
             num[i][1] = Integer.parseInt(input[1]);
             num[i][2] = Integer.parseInt(input[2]);
-        }  	
+        }
         boolean flagA, flagB, flagC;
         for(int i=0; i<N-1; i++) {
-            flagA = flagB = flagC = false;
+            flagA = false; flagB = false; flagC = false;
             for(int j=i+1; j<N; j++) {
-                if(num[i][0]==num[j][0]){
+                if(num[i][0]==num[j][0]) {
                     flagA = true;
                     num[j][0] = 0;
                 }
-                if(num[i][1]==num[j][1]){
+                if(num[i][1]==num[j][1]) {
                     flagB = true;
                     num[j][1] = 0;
                 }
-                if(num[i][2]==num[j][2]){
+                if(num[i][2]==num[j][2]) {
                     flagC = true;
                     num[j][2] = 0;
                 }
@@ -31,7 +31,7 @@ class Main {
             if(flagB) num[i][1] = 0;
             if(flagC) num[i][2] = 0;
             System.out.println(num[i][0] + num[i][1] + num[i][2]);
-            if(i==N-2) System.out.println(num[i+1][0] + num[i+1][1] + num[i+1][2]);
         }
+        System.out.println(num[N-1][0] + num[N-1][1] + num[N-1][2]);
     }
 }
