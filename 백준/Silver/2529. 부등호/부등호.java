55459162e0e49e6f9fca2b8f3590a8761763b3a7
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-07-06 18:57:21 +0900

    [Silver I] Title: 부등호, Time: 1644 ms, Memory: 29480 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,51 +1,57 @@
 import java.io.*;
 import java.util.*;
-class Main {
-	static List<Integer> list = new ArrayList<>();
-	static List<String> result = new ArrayList<>();
-	static String[] signs;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int K = Integer.parseInt(br.readLine());
-		signs = br.readLine().split(" ");
-		recur(0, K+1);
-		System.out.println(result.get(result.size()-1));
-		System.out.println(result.get(0));
-	}
-	private static void recur(int start, int end) {
-		if(start == end) {
-			boolean flag = true;
-			for(int i=0; i<signs.length; i++) {
-				if(signs[i].equals("<")) {
-					if(list.get(i)<list.get(i+1)) continue;
-					else {
-						flag = false;
-						break;
-					}
-				} else {
-					if(list.get(i)>list.get(i+1)) continue;
-					else {
+class Main{
+    static List<Integer> list = new ArrayList<>();
+    static List<String> result = new ArrayList<>();
+    static String[] signs;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        signs = br.readLine().split(" ");
+        recur(0, K+1); //숫자의 개수 : 부등호 +1
+        System.out.println(result.get(result.size()-1));
+        System.out.println(result.get(0));
+    }
+    private static void recur(int start, int end) {
+        if(start == end){
+            //K = 2라면 -> end가 3이자나요 3일 때 멈춰야함.
+            boolean flag = true;
+            for(int i=0; i<signs.length; i++){
+                //나는 지금 sign으로 비교할거야 
+                //어떻게? sign기준으로 왼쪽 오른쪽 숫자를 비교하면 됨.
+                if(signs[i].equals("<")) {
+                    if(list.get(i)<list.get(i+1)) {
+                        continue;
+                    } else {
                         flag = false;
-						break;
-					}
-				}
-			}
-			if(flag) {
-				Iterator<Integer> iter = list.iterator();
-				StringBuilder sb = new StringBuilder();
-				while(iter.hasNext()) {
-					sb.append(iter.next());
-				}
-				result.add(sb.toString());
-			}
-			return;
-		}
-		for(int i=0; i<=9; i++) {
-			if(list.contains(i)) continue;
-			list.add(i);
-			recur(start+1, end);
-			list.remove(list.indexOf(i));
-		}
-	}
+                        break;
+                    }
+                } else {
+                      if(list.get(i)>list.get(i+1)) {
+                        continue;
+                    } else {
+                        flag = false;
+                        break;
+                    }
+                }
+            } 
+            if(flag) {
+                Iterator<Integer> iter = list.iterator();
+                StringBuilder sb = new StringBuilder();
+                while(iter.hasNext()) {
+                    sb.append(iter.next());
+                }
+                result.add(sb.toString());  
+            }
+            return;
+        }
+        for(int i=0; i<=9; i++){
+            if(list.contains(i)) {
+                continue;
+            }
+            list.add(i);
+            recur(start+1, end);
+            list.remove(list.indexOf(i));
+        }
+    }
 }
