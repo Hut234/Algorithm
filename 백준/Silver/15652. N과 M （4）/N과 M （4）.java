49f59b4c194f27546a9ebe339c21acd68736dfc6
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-07-07 21:49:24 +0900

    [Silver III] Title: N과 M (4), Time: 324 ms, Memory: 20644 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class Main {
+	static int N, M;
+	static Stack<Integer> STACK = new Stack<>();
+	static StringBuilder SB = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		String[] input = (new BufferedReader(new InputStreamReader(System.in))).readLine().split(" ");
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+		recur(0);
+	}
+	private static void recur(int idx) {
+		if(idx==M) {
+			Iterator<Integer> iter = STACK.iterator();
+			while(iter.hasNext()) SB.append(iter.next()).append(" ");
+			System.out.println(SB.toString());
+			SB.setLength(0);
+			return;
+		}
+		for(int i=1; i<=N; i++) {
+			if(!STACK.isEmpty()) if(STACK.peek() > i) continue;
+			STACK.push(i);
+			recur(idx+1);
+			STACK.pop();
+		}
+	}
+}
