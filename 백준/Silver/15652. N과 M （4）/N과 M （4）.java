2d11caf3edde9eb88c927a46beff252b4658ebd0
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-07-08 14:29:41 +0900

    [Silver III] Title: N과 M (4), Time: 92 ms, Memory: 13104 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,28 +1,30 @@
 import java.io.*;
-class Main {
+class Main{
     static int N, M;
     static int[] ARR;
     static StringBuilder SB = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
+    public static void main(String[] args) throws IOException{
+        String[] input = (new BufferedReader(new InputStreamReader(System.in))).readLine().split(" ");
         N = Integer.parseInt(input[0]);
         M = Integer.parseInt(input[1]);
         ARR = new int[M];
         recur(0, 0);
         System.out.println(SB.toString());
-    }   
-    
+    }
     private static void recur(int idx, int prev) {
-    	if(idx == M) {
-    		for(int i=0; i<ARR.length; i++) SB.append(ARR[i]).append(" ");
-    		SB.append("\n");
-    		return;
-    	}
-    	for(int i=1; i<=N; i++) {
-    		if(prev>i) continue;
-    		ARR[idx] = i; 
-    		recur(idx+1, ARR[idx]);
-    	}
+        if(idx == M) {
+            for(int i=0; i<ARR.length; i++){
+                SB.append(ARR[i]).append(" ");
+            }
+            SB.append("\n");
+            return;
+        }
+        for(int i=1; i<=N; i++) {
+            if(prev>i) {
+                continue;
+            }
+            ARR[idx] = i;
+            recur(idx+1, ARR[idx]);
+        }        
     }
 }
