865f3a9f7e58c12192787afa87eb0692941b11d7
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-07-08 13:46:17 +0900

    [Silver III] Title: N과 M (4), Time: 108 ms, Memory: 15140 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,28 +1,29 @@
 import java.io.*;
-import java.util.*;
 class Main {
-	static int N, M;
-	static Stack<Integer> STACK = new Stack<>();
-	static StringBuilder SB = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		String[] input = (new BufferedReader(new InputStreamReader(System.in))).readLine().split(" ");
-		N = Integer.parseInt(input[0]);
-		M = Integer.parseInt(input[1]);
-		recur(0);
-	}
-	private static void recur(int idx) {
-		if(idx==M) {
-			Iterator<Integer> iter = STACK.iterator();
-			while(iter.hasNext()) SB.append(iter.next()).append(" ");
-			System.out.println(SB.toString());
-			SB.setLength(0);
-			return;
-		}
-		for(int i=1; i<=N; i++) {
-			if(!STACK.isEmpty()) if(STACK.peek() > i) continue;
-			STACK.push(i);
-			recur(idx+1);
-			STACK.pop();
-		}
-	}
+    static int N, M;
+    static int[] ARR;
+    static StringBuilder SB = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        ARR = new int[M];
+        recur(0, 0);
+        System.out.println(SB.toString());
+    }   
+    
+    private static void recur(int idx, int prev) {
+    	if(idx == M) {
+    		for(int i=0; i<ARR.length; i++) SB.append(ARR[i]).append(" ");
+    		SB.append("\n");
+    		return;
+    	}
+    	for(int i=1; i<=N; i++) {
+    		if(prev>i) continue;
+    		ARR[idx] = i; 
+    		recur(idx+1, ARR[idx]);
+    		ARR[idx] = 0;
+    	}
+    }
 }
