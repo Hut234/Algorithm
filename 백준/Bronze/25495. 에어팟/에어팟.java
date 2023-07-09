f75a522893c25258c2082c1f036331fef7bf6dc1
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-07-09 18:15:44 +0900

    [Bronze III] Title: 에어팟, Time: 76 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Bronze/25495. 에어팟/에어팟.java
+++ 백준/Bronze/25495. 에어팟/에어팟.java
@@ -1,26 +1,28 @@
 import java.io.*;
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String[] input = br.readLine().split(" ");
-		int sum = 0, val, prev = 0, prevSum = 0;
-		for(int i=0; i<N; i++) {
-			val = Integer.parseInt(input[i]);
-			if(val == prev) {
-				prevSum *= 2;
-				sum += prevSum;
-			} else {
-				prevSum = 2;
-				sum += 2;
-			}
-			if(sum>=100) {
-				prev = 0;
-				sum = 0;
-			} else {
-				prev = val;
-			}
-		}
-		System.out.println(sum);
-	}
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int result = 0, val, prev=0, prevAmount=0;
+        for(int i=0; i<N; i++) {
+            val = Integer.parseInt(input[i]);
+            //1. 이전 휴대폰이랑 지금 휴대폰이랑 같은지 비교
+            if(val == prev) {//같으면 이전 소모량 곱하기 두배해서 더하기
+                prevAmount *= 2;
+                result += prevAmount;
+            } else {//다르면 2를 더하기
+                prevAmount = 2;
+                result += 2;
+            }
+            
+            if(result >= 100) {//result 총배터리 사용량이 100을 넘으면 완충하고 진행
+                prev = 0;
+                result = 0;
+            } else {//안 넘으면 누적해서 더하기.
+                prev = val;
+            }
+        }
+        System.out.print(result);
+    }
 }
