cb4e6600ab66256b35ec924760488c63c3b8d36d
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-08-13 04:38:30 +0900

    [Gold V] Title: 집합의 표현, Time: 1344 ms, Memory: 28716 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,32 +1,46 @@
-import java.io.*;
-import java.util.*;
 class Main {
 	static int[] graph;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] nm = br.readLine().split(" ");
-		int n = Integer.parseInt(nm[0]);
-		int m = Integer.parseInt(nm[1]);
+	static int[] rank;
+	public static void main(String[] args) throws Exception {
+		int n = read();
+		int m = read();
 		
 		graph = new int[n+1];
-		for(int i=0; i<=n; i++) graph[i] = i;
-		
-		StringTokenizer st;
+		rank = new int[n+1];
+		for(int i=0; i<=n; i++) {
+			graph[i] = i;
+			rank[i] = 1;
+		}
 		int o, a, b;
 		while(m-->0) {
-			st = new StringTokenizer(br.readLine());
-			o = Integer.parseInt(st.nextToken());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			o = read();
+			a = read();
+			b = read();
 			if(o==0) union(a, b);
 			else System.out.println(find(a)==find(b) ? "YES" : "NO");
 		}
+		
 	}
 	static void union(int a, int b) {
-		graph[find(a)] = find(b);
+		int findA = find(a);
+		int findB = find(b);
+		if(findA==findB) return;
+		if(rank[a]==rank[b]) {
+			graph[findA] = findB;
+			rank[b] += 1;
+		} else if(rank[a]>rank[b]) graph[findB] = findA;
+		else graph[findA] = findB;
 	}
 	static int find(int val) {
 		if(graph[val]==val) return val;
 		else return graph[val] = find(graph[val]);
 	}
+    static int read() throws Exception {
+        int c, n = 0;
+        while (true) {
+            c = System.in.read() - 48;
+            if (c < 0 || c > 9) return n;
+            n = n * 10 + c;
+        }
+    }
 }
