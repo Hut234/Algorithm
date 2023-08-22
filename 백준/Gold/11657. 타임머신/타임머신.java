b71fc9d378e18989449d4c9260ae0007e7e14814
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-08-22 16:45:09 +0900

    [Gold IV] Title: 타임머신, Time: 128 ms, Memory: 12444 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,31 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
 class Main {
-	static long[] dist;
-    static List<Edge> al = new ArrayList<>();
 	public static void main(String[] args) throws Exception {
 		int N = read();
 		int M = read();
-		dist = new long[N+1];
+		long[] dist = new long[N+1];
 		for(int i=1; i<=N; i++) dist[i] = Integer.MAX_VALUE;
+		List<Edge> al = new ArrayList<>();
 		for(int i=0; i<M; i++) al.add(new Edge(read(), read(), read()));
 		dist[1] = 0;
 		Edge cur;
-		for(int i=0; i<N-1; i++) {
+		boolean cycle = false;
+		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
 				cur = al.get(j);
 				if(dist[cur.start]!=Integer.MAX_VALUE) {
 					long tmp = dist[cur.start] + cur.weight;
-					if(dist[cur.end]>tmp) dist[cur.end] = tmp;
+					if(dist[cur.end]>tmp) {
+						if(i==N-1) cycle = true;
+						dist[cur.end] = tmp;
+					}
 				}
-			}
-		}
-		boolean cycle = false;
-		for(int j=0; j<M; j++) {
-			cur = al.get(j);
-			if(dist[cur.start]!=Integer.MAX_VALUE) {
-				long tmp = dist[cur.start] + cur.weight;
-				if(dist[cur.end]>tmp) cycle = true;
 			}
 		}
 		if(cycle) {
@@ -49,6 +44,7 @@ class Main {
 				negative = true;
 				c = System.in.read();
 			}
+			if(c==13) System.in.read();
 			if(c<'0'||c>'9') return negative ? -n : n;
 			n = n*10 +(c-'0');
 		}
