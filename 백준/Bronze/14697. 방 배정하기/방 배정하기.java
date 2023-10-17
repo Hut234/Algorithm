0ce31d6e8a3e8435ccadfee230684c23a865e3b8
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-10-17 12:45:22 +0900

    [Bronze II] Title: 방 배정하기, Time: 76 ms, Memory: 11520 KB, Score: 22 point -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int A = Integer.parseInt(s[0]);
+        int B = Integer.parseInt(s[1]);
+        int C = Integer.parseInt(s[2]);
+        int N = Integer.parseInt(s[3]);
+        if(A==1) {
+            System.out.print(1);
+            return;
+        }
+        if(B%A==0 && C%A == 0) {
+            if(N%A==0) {
+                System.out.print(1);
+            } else {
+                System.out.print(0);
+            }
+            return;
+        }
+        int tmpA, tmpB, tmpC;
+        for(int i=0; i*A<300; i++) {
+            tmpA = i*A;
+            for(int j=0; j*B+tmpA<300; j++) {
+                tmpB = j*B;
+                for(int k=0; k*C+tmpA+tmpB<300; k++) {
+                    tmpC = k*C;
+                    if(tmpA+tmpB+tmpC==N) {
+                        System.out.println(1);
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(0);
+    }
+}
