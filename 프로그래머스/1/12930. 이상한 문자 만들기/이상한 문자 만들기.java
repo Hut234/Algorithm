3744f1b970a852151e2a783bae20d61f40621484
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-11-15 10:49:44 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.08 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,23 +1,22 @@
 class Solution {
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
-        String[] words = s.split(" ", -1);
-        String word;
         char c;
-        for (int i = 0; i < words.length; i++) {
-            word = words[i];
+        int idx = 0;
+        for (int i = 0; i<s.length(); i++) {
+            c = s.charAt(i);
 
-            if(word.length()!=0) {
-                for (int j = 0; j < word.length(); j++) {
-                    c = word.charAt(j);
-                    if (j % 2 == 0) {
-                        sb.append((char) (c>=97 ? c-32 : c));
-                    } else {
-                        sb.append((char) (c<97 ? c+32 : c));
-                    }
+            if(c==32) {
+                idx = 0;
+                sb.append(" ");
+            } else {
+                if(idx%2==0) {
+                    sb.append( (char)(c>=97 ? c-32 : c) );
+                } else {
+                    sb.append( (char)(c<97 ? c+32 : c) );
                 }
+                idx++;
             }
-            if(i!= words.length-1) sb.append(" ");
         }
         return sb.toString();
     }
