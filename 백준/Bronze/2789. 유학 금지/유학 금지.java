eb07df719b70ba49f4ef23b0f9eea61deadfba1d
Hut234 <135043156+Hut234@users.noreply.github.com>
2023-11-22 20:31:06 +0900

    [Bronze II] Title: 유학 금지, Time: 112 ms, Memory: 12744 KB -BaekjoonHub

---
+++ 백준/Bronze/2789. 유학 금지/유학 금지.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String[] args){
+        String s = new java.util.Scanner(System.in).next();
+        System.out.print(s.replaceAll("[CAMBRIDGE]", ""));
+    }
+}
