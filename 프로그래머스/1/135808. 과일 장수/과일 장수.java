89edd92516b3aa90f96c23c6eca1e5361b73ae7e
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-03-07 23:14:36 +0900

    [level 1] Title: 과일 장수, Time: 0.40 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,22 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int k, int m, int[] score) {
-
-        // k: 최대 점수
-        // m: 한 상자의 사과 수
-        // score: 사과 배열
-        
-        Arrays.sort(score);
-        
         int answer = 0;
-        // 1 1 1 2 2 3 3
-        // 1 1 2 2 2 2 4 4 4 4 4 4
-        for(int i=score.length-m; i>-1; i-=m) {
-            answer += score[i]*m;
-        }
-        
-        // 결과: k * m * 상자 수
         return answer;
     }
 }
