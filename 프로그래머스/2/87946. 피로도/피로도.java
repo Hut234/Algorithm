64486a50c8ecdadbb145981adfc77c3f73c36c92
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-09-06 02:03:40 +0900

    [level 2] Title: 피로도, Time: 2327.31 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,31 +1,33 @@
 class Solution {
-    static int k;
-	static int len;
-	static int[][] tmp;
-	static int result;
-	static boolean[] visited;
+    private static int length;
+    private static int result;
+    private static int[][] dungeons;
+    private static boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
-    	Solution.k = k;
-    	Solution.len = dungeons.length;
-    	Solution.tmp = dungeons;
-    	Solution.visited = new boolean[len];
-    	recur(0, k, 0);
+        Solution.length = dungeons.length;
+        Solution.dungeons = dungeons;
+        Solution.visited = new boolean[length];
+
+        recur(0, k, 0);
         return result;
     }
-    
-    private static void recur(int idx, int p, int cnt) {
-    	if (p < 1) return;
-    	if (result < cnt) result = Integer.max(result, cnt);
-    	if (idx == len) return;
-    	
-    	for (int i = 0; i < len; i++) {
-    		if(!visited[i] && tmp[i][0] <= p) {
-    			visited[i] = true;
-    			recur(idx+1, p-tmp[i][1], cnt+1);
-    			visited[i] = false;
-    			recur(idx+1, p, cnt);
-    		}
-    	}
+
+    private static void recur(int idx, int fatigue, int cnt){
+        // 피로도가 1미만
+        if (fatigue < 1) return;
+        // 방문수
+        if (cnt > result) result = cnt;
+        // base case
+        if (idx == length) return;
+
+        for (int i = 0; i < length; i++) {
+            if (!visited[i] && dungeons[i][0] <= fatigue) {
+                visited[i] = true;
+                recur(idx+1, fatigue -dungeons[i][1], cnt+1);
+                visited[i] = false;
+                recur(idx+1, fatigue, cnt);
+            }
+        }
     }
 }
