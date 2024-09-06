aa1f7cf649fb3435f5257fcc55a51035266f93b6
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-09-06 19:34:33 +0900

    [Silver III] Title: 숫자 야구, Time: 64 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,45 +1,73 @@
 import java.io.*;
 import java.util.*;
+
 class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()), n = N;
-		StringTokenizer st;
-		String[] val = new String[N]; int[] strk = new int[N], ball = new int[N]; 
-		int idx=0, strkCnt, ballCnt, ans=0;
-		boolean flag;
-		while(n-->0) {
-			st = new StringTokenizer(br.readLine());
-			val[idx] = st.nextToken();
-			strk[idx] = Integer.parseInt(st.nextToken());
-			ball[idx++] = Integer.parseInt(st.nextToken());
-		}
-		for(int i=1; i<10; i++) {
-			for(int j=1; j<10; j++) {
-				if(i==j) continue;
-				for(int k=1; k<10; k++) {
-					if(i==k||j==k) continue;
-					flag = true;
-					for(int l=0; l<N; l++) {
-						strkCnt = 0; ballCnt = 0;
-						int a = val[l].charAt(0)-'0';
-						int b = val[l].charAt(1)-'0';
-						int c = val[l].charAt(2)-'0';
-						if(a==i) strkCnt++;
-						if(b==j) strkCnt++;
-						if(c==k) strkCnt++;
-						if(i!=a&&i==b||i==c) ballCnt++;
-						if(j!=b&&j==a||j==c) ballCnt++;
-						if(k!=c&&k==a||k==b) ballCnt++;
-						if(strk[l]!=strkCnt||ball[l]!=ballCnt) {
-							flag =false;
-							break;
-						}
-					}
-					if(flag) ans++;
-				}
-			}
-		}
-		System.out.println(ans);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        NumBall[] numBalls = new NumBall[N];
+
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            numBalls[i] = new NumBall(st.nextToken(), st.nextToken(), st.nextToken());
+        }
+
+
+        int result = 0;
+        for (int h = 1; h <= 9; h++) {
+            for (int t = 1; t <= 9; t++) {
+                if (t == h) continue;
+                for (int u = 1; u <= 9; u++) {
+                    if (u == h) continue;
+                    if (u == t) continue;;
+
+                    boolean flag = true;
+                    for (int i = 0; i < N; i++) {
+                        NumBall numBall = numBalls[i];
+
+                        int strikeCount = 0;
+                        if (numBall.hundred == h) strikeCount++;
+                        if (numBall.ten == t) strikeCount++;
+                        if (numBall.unit == u) strikeCount++;
+
+                        int ballCount = 0;
+                        if (numBall.ten == h || numBall.unit == h) ballCount++;
+                        if (numBall.hundred == t || numBall.unit == t) ballCount++;
+                        if (numBall.hundred == u || numBall.ten == u) ballCount++;
+
+                        if (numBall.strikeCount != strikeCount || numBall.ballCount != ballCount) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag) result++;
+                }
+            }
+        }
+
+        System.out.print(result);
+    }
+
+    private static class NumBall {
+        int unit;
+        int ten;
+        int hundred;
+        int strikeCount;
+        int ballCount;
+
+        private NumBall(String value, String strike, String ball) {
+            int val = Integer.parseInt(value);
+            this.unit = val %10;
+            this.ten = val /10 %10;
+            this.hundred = val /100;
+            this.strikeCount = Integer.parseInt(strike);
+            this.ballCount = Integer.parseInt(ball);
+        }
+
+    }
+
 }
