7276162c09b0aafa4b1d22caa88e56d86a746ebe
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-09-09 22:23:03 +0900

    [Silver III] Title: 두 수의 합, Time: 184 ms, Memory: 26144 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,24 +1,30 @@
 import java.io.*;
 import java.util.*;
+
 class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
-		Arrays.sort(arr);
-		int X = Integer.parseInt(br.readLine());
-		int s = 0, e = N-1, cnt = 0;
-		while(s<e) {
-			if(arr[s] + arr[e] == X) {
-				cnt++;
-				s++;
-				e--;
-			} 
-			else if(arr[s] + arr[e] > X) e--; 
-			else s++;
-		}
-		System.out.println(cnt);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        boolean[] exist = new boolean[1000001];
+
+        int[] numbers = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            numbers[i] = cur;
+            exist[cur] = true;
+        }
+
+        int X = Integer.parseInt(br.readLine());
+
+        int result = 0;
+        int minus;
+        for (int i = 0; i < N; i++) {
+            minus = X -numbers[i];
+            if (minus > 0 && minus < 1000001 && exist[minus]) result++;
+        }
+
+        System.out.print(result/2);
+    }
 }
