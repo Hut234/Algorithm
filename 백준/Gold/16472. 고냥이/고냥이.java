994cb7cc545f404b979ae59930c655db1d78fcde
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-09-10 20:54:16 +0900

    [Gold IV] Title: 고냥이, Time: 180 ms, Memory: 30360 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,30 +1,39 @@
 import java.io.*;
 import java.util.*;
-class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()), max = 0;
-        String str = br.readLine();
-        Set<Character> set = new HashSet<>();
-        for(int s=0, e=0; s<str.length()&&e<str.length();) {
-            if(set.size() > N) {
-                max = Integer.max(max, e-s-1);                
-                s++;
-                e = s; 
-                set.clear();
-            } else {
-                set.add(str.charAt(e));
-                e++;
-            }
-            if(e==str.length()) {
-                if(set.size()>N) {
-                    max = Integer.max(max, e-1-s);
-                } else {
-                    max = Integer.max(max, e-s);
-                }
-                  break;
-            }
-        }
-        System.out.print(max);
-    }
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		int max = 0, size, newS = -1;
+		boolean isFirst = true;
+		Set<Character> set = new HashSet<>();
+		for(int s=0, e=0; s<str.length() && e<str.length();) {
+			set.add(str.charAt(e));
+			size = set.size();
+			if(size>N) {
+				max = Integer.max(max, e-s);
+				if(newS != -1) {
+					s = e = newS;
+					newS = -1;
+					isFirst = true;
+				} else {
+					s++;
+					e = s;
+				}
+				set.clear();
+			} else {
+				if(size==2&&isFirst) {
+					newS = e;
+					isFirst = false;
+				}
+				e++;
+				if(e==str.length()) {
+					max = Integer.max(max, e-s);
+					break;
+				}
+			}
+		}
+		System.out.print(max);
+	}
 }
