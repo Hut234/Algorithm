8dd82031126dfa34c63d2d67b92d6f08ec817e1a
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-09-12 20:57:02 +0900

    [Silver V] Title: 숫자 카드, Time: 596 ms, Memory: 154752 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,21 +1,33 @@
 import java.io.*;
 import java.util.*;
+
 class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String[] standard = br.readLine().split(" ");
-		int[] std = new int[N];
-		int M = Integer.parseInt(br.readLine());
-		String[] input = br.readLine().split(" ");
-		for(int i=0; i<N; i++) std[i] = Integer.parseInt(standard[i]);
-		Arrays.sort(std);
-		int[] result = new int[M];
-		int tmp;
-		for(int i=0; i<M; i++) {
-			tmp = Arrays.binarySearch(std, Integer.parseInt(input[i]));
-			result[i] = tmp < 0 ? 0 : 1; 
-		}
-		for(int val : result) System.out.print(val + " ");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        boolean[] isExisted = new boolean[20_000_000];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            isExisted[Integer.parseInt(st.nextToken()) +10_000_000] = true;
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        int[] comparingCards = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            comparingCards[i] = Integer.parseInt(st.nextToken()) +10_000_000;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            if (isExisted[comparingCards[i]]) sb.append("1");
+            else sb.append("0");
+            sb.append(" ");
+        }
+
+        System.out.print(sb);
+    }
 }
