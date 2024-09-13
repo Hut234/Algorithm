90a226f527b532e5f890dc2d6ad63be38a4e2328
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-09-13 20:20:53 +0900

    [Silver III] Title: 수열, Time: 160 ms, Memory: 21608 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,30 +1,25 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N+1];
-		int idx = 1;
-		
-		st = new StringTokenizer(br.readLine());
-		while (st.hasMoreTokens()) {
-			arr[idx] = arr[idx-1] + Integer.parseInt(st.nextToken());
-			idx++;
-		}
-		
-		int max = -10000001;
-		for (int i = K; i <= N; i++) {
-			max = Integer.max(max, (arr[i]-arr[i-K]));
-		}
-		
-		System.out.print(max);
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
+        st = new StringTokenizer(br.readLine());
+        int[] sum = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        int max = -10000001;
+        for (int i = K, j = 0; i <= N; i++, j++) {
+            max = Integer.max(max, sum[i] -sum[j]);
+        }
+
+        System.out.print(max);
+    }
 }
