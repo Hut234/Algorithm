f0b4e2cf8833563c0adb75eb84fcf68fb158fd1c
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-09-20 05:16:42 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 60 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,27 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = 9;
-		int[] arr = new int[N];
-		int sum = 0;
-		for(int i=0; i<9; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			sum += arr[i];
-		}
-		Arrays.sort(arr);
-		for(int i=0; i<N-1; i++) {
-			for(int j=i+1; j<N; j++) {
-				if(sum-(arr[i]+arr[j]) == 100) {
-					for(int p=0; p<N; p++) {
-						if(p==i||p==j) continue;
-						System.out.println(arr[p]);
-					}
-                    return;
-				}
-			}
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        int[] heights = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            int height = Integer.parseInt(st.nextToken());
+            sum += height;
+            heights[i] = height;
+        }
+
+        Arrays.sort(heights);
+
+        int wrongDwarf1 = -1, wrongDwarf2 = -1;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == j) continue;
+
+                if (sum -heights[i] -heights[j] == 100) {
+                    wrongDwarf1 = i;
+                    wrongDwarf2 = j;
+                    break;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            if (i == wrongDwarf1 || i == wrongDwarf2) continue;
+            sb.append(heights[i]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
 }
