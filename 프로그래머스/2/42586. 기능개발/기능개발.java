8de02dc3c7b04433838e6b1be40e688ac20fd02a
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-10-23 18:54:38 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,21 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        int[] arr = new int[progresses.length];
+        int[] remainProgresses = new int[progresses.length];
         for (int i = 0; i < progresses.length; i++) {
-            arr[i] = 100 -progresses[i];
+            remainProgresses[i] = 100 -progresses[i];
         }
         
-        int[] arr2 = new int[progresses.length];
+        int[] needDays = new int[progresses.length];
         List<Integer> result = new ArrayList<>();
         int tmp = -1, cur = -1;
         for (int i = 0; i < progresses.length; i++) {
-            if (arr[i] %speeds[i] != 0) arr2[i] = arr[i] /speeds[i] +1;
-            else arr2[i] = arr[i] /speeds[i];
+            if (remainProgresses[i] %speeds[i] != 0) needDays[i] = remainProgresses[i] /speeds[i] +1;
+            else needDays[i] = remainProgresses[i] /speeds[i];
 
-            if (arr2[i] > tmp) {
+            if (needDays[i] > tmp) {
                 cur++;
-                tmp = arr2[i];
+                tmp = needDays[i];
                 result.add(1);
             }
             else result.set(cur, result.get(cur)+1);
