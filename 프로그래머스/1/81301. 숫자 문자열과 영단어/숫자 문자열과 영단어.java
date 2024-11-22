8c5b8947a41a2c395fffdeadcde9b3bbda279884
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-11-22 10:49:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.13 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,42 +1,11 @@
-import java.util.*;
 class Solution {
     public int solution(String s) {
-        Map<String, Integer> numMap = new HashMap<>();
-        numMap.put("zero", 0);
-        numMap.put("one", 1);
-        numMap.put("two", 2);
-        numMap.put("three", 3);
-        numMap.put("four", 4);
-        numMap.put("five", 5);
-        numMap.put("six", 6);
-        numMap.put("seven", 7);
-        numMap.put("eight", 8);
-        numMap.put("nine", 9);
-
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length();) {
-            char c = s.charAt(i);
-
-            if (c > 57) {
-                String str = s.substring(i, i+3);
-                if (numMap.containsKey(str)) {
-                    sb.append(numMap.get(str));
-                    i += 3;
-                } else {
-                    for(int lastIdx=4; i+lastIdx<=s.length(); lastIdx++) {
-                        str = s.substring(i, i +lastIdx);
-                        if (numMap.containsKey(str)) {
-                            sb.append(numMap.get(str));
-                            i += lastIdx;
-                            break;
-                        }
-                    }
-                }
-            } else {
-                sb.append(c);
-                i++;
-            }
+        String[] numStr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String result = s.replace(numStr[0], "0");
+        for (int i=1; i<10; i++) {
+            result = result.replace(numStr[i], String.valueOf(i));
         }
-        return Integer.parseInt(sb.toString());
+        
+        return Integer.parseInt(result);
     }
 }
