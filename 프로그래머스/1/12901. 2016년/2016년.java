67d11dcc95da17711608c8a3e839daf5b95784b5
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-11-26 10:31:30 +0900

    [level 1] Title: 2016년, Time: 0.05 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,37 +1,11 @@
-import java.util.*;
 class Solution {
     public String solution(int a, int b) {
-        Map<Integer, String> days = new HashMap<>();
-        days.put(0, "FRI");
-        days.put(1, "SAT");
-        days.put(2, "SUN");
-        days.put(3, "MON");
-        days.put(4, "TUE");
-        days.put(5, "WED");
-        days.put(6, "THU");
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
         
-        
-        Map<Integer, Integer> months = new HashMap<>();
-        months.put(1, 31);
-        months.put(2, 29);
-        months.put(3, 31);
-        months.put(4, 30);
-        months.put(5, 31);
-        months.put(6, 30);
-        months.put(7, 31);
-        months.put(8, 31);
-        months.put(9, 30);
-        months.put(10, 31);
-        months.put(11, 30);
-        
-        a -= 1;
         int answer = b-1;
-        for(int i=1; i<=a; i++) {
-            answer += months.get(i);
-        }
-        
-        
-        
-        return days.get(answer%7);
+        for(int i=1; i<=a-1; i++) answer += months[i];
+
+        return days[answer%7];
     }
 }
