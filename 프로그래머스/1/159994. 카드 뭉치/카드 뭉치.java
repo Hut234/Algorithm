92737e1f796310f0c57d5a5e3fa8ad3efef43725
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-11-27 11:22:34 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,32 +1,19 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        StringBuilder sb = new StringBuilder();
-
-        int start = 0;
-        if (goal[0].equals(cards2[0])) start = 1;
-
-        for (int c1=0, c2=0, g=0, cnt=0; cnt<2&&g<goal.length; start++) {
-            if (start%2 == 0) {
-                for (; c1<cards1.length&&g<goal.length; c1++, g++) {
-                    if (!goal[g].equals(cards1[c1])) {
-                        cnt++;
-                        break;
-                    }
-                    sb.append(cards1[c1]);
-                    cnt = 0;
-                }
+        int c1 = 0;
+        int c2 = 0;
+        
+        String answer = "Yes";
+        for (String str : goal) {
+            if (c1<cards1.length&&str.equals(cards1[c1])) {
+                c1++;
+            } else if (c2<cards2.length&&str.equals(cards2[c2])) {
+                c2++;
             } else {
-                for (; c2<cards2.length&&g<goal.length; c2++, g++) {
-                    if (!goal[g].equals(cards2[c2])) {
-                        cnt++;
-                        break;
-                    }
-                    sb.append(cards2[c2]);
-                    cnt = 0;
-                }
+                answer = "No";
+                break;
             }
         }
-        
-        return sb.toString().equals(String.join("", goal)) ? "Yes" : "No";
+        return answer;
     }
 }
