58bc4f5b3ebd789cb7aafbab5427ef32563c3aa9
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-11-29 10:26:57 +0900

    [level 1] Title: 덧칠하기, Time: 1.42 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        
-        boolean[] paint = new boolean[n+1];
-        for(int i=0; i<section.length; i++) {
-            if(!paint[section[i]]) {
-                for(int j=section[i]; j<=n&&j<section[i]+m; j++) {
-                    paint[j] = true;
-                }
+        for(int i=0, cur=0; i<section.length; i++) {
+            if (cur<section[i]) {
                 answer++;
-            }            
+                cur = section[i] +m -1;
+            }
         }
         
         return answer;
