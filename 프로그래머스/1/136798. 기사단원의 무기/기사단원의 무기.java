630f209875b5aec412439689a1018f7941f52e0a
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-12-02 10:40:25 +0900

    [level 1] Title: 기사단원의 무기, Time: 108.46 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 1;
+        for(int i=2; i<=number; i++) {
+            int cnt = 0;
+            for(int j=1; j*j<=i; j++) {
+                if(i%j==0) {
+                    if(j*j==i) cnt++;
+                    else cnt+=2;
+                }
+            }
+            answer += cnt>limit ? power : cnt;
+        }
+        
+        return answer;
+    }
+}
