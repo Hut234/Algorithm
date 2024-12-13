463d37fbb325e0d693b3a4cfb962910fae171381
Hut234 <135043156+Hut234@users.noreply.github.com>
2024-12-13 18:14:22 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.12 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<my_string.length(); i++) {
+            int val = my_string.charAt(i);
+
+            if (val > 94) sb.append((char)(val-32));
+            else sb.append((char)(val+32));
+        }
+        return sb.toString();
+    }
+}
