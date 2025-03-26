d16bfc2e030f37584c25a334aaf36c3ffbeeba19
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-03-26 16:18:11 +0900

    [Gold IV] Title: 알파벳, Time: 700 ms, Memory: 12052 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
-
     static int r, c, result;
     static char[][] alphabets;
     static int[] dr = {0, 0, 1, -1};
@@ -22,23 +19,23 @@ public class Main {
             alphabets[i] = br.readLine().toCharArray();
         }
 
-        Set<Character> visited = new HashSet<>();
-        visited.add(alphabets[0][0]);
+        int visited = 1 << (alphabets[0][0] - 'A');
         dfs(0, 0, visited, 1);
         System.out.print(result);
     }
 
-    static void dfs(int row, int col, Set<Character> visited, int depth) {
+    static void dfs(int row, int col, int visited, int depth) {
         result = Math.max(result, depth);
 
         for (int i = 0; i < 4; i++) {
             int nr = row + dr[i];
             int nc = col + dc[i];
 
-            if (nr >= 0 && nr < r && nc >= 0 && nc < c && !visited.contains(alphabets[nr][nc])) {
-                visited.add(alphabets[nr][nc]);
-                dfs(nr, nc, visited, depth + 1);
-                visited.remove(alphabets[nr][nc]);
+            if (nr >= 0 && nr < r && nc >= 0 && nc < c) {
+                int alpha = alphabets[nr][nc] - 'A';
+                if ((visited & (1 << alpha)) == 0) { 
+                    dfs(nr, nc, visited | (1 << alpha), depth + 1);
+                }
             }
         }
     }
