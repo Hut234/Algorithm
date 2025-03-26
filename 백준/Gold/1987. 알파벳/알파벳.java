d90c697a5e0f31a7cba8aec7c520103bd61e76b3
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-03-26 16:03:46 +0900

    [Gold IV] Title: 알파벳, Time: 1976 ms, Memory: 296464 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,47 +1,45 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
-    static int r;
-    static int c;
-    static String[][] alphabets;
-    static int[][] counts;
-    static int result;
+    static int r, c, result;
+    static char[][] alphabets;
+    static int[] dr = {0, 0, 1, -1};
+    static int[] dc = {1, -1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String[] rc = br.readLine().split(" ");
         r = Integer.parseInt(rc[0]);
         c = Integer.parseInt(rc[1]);
 
-        alphabets = new String[r][c];
-        counts = new int[r][c];
+        alphabets = new char[r][c];
         for (int i = 0; i < r; i++) {
-            String[] inputs = br.readLine().split("");
-            System.arraycopy(inputs, 0, alphabets[i], 0, inputs.length);
+            alphabets[i] = br.readLine().toCharArray();
         }
 
-        recursion(0, 0, new HashMap<>());
+        Set<Character> visited = new HashSet<>();
+        visited.add(alphabets[0][0]);
+        dfs(0, 0, visited, 1);
         System.out.print(result);
     }
 
-    static void recursion(int row, int column, Map<String, Integer> collect) {
-        if (row < 0 || row == r) return;
-        if (column < 0 || column == c) return;
-        if (collect.containsKey(alphabets[row][column])) return;
+    static void dfs(int row, int col, Set<Character> visited, int depth) {
+        result = Math.max(result, depth);
 
-        collect.put(alphabets[row][column], 1);
-        result = Integer.max(collect.size(), result);
+        for (int i = 0; i < 4; i++) {
+            int nr = row + dr[i];
+            int nc = col + dc[i];
 
-        recursion(row, column + 1, collect);
-        recursion(row + 1, column, collect);
-        recursion(row - 1, column, collect);
-        recursion(row, column - 1, collect);
-        collect.remove(alphabets[row][column]);
+            if (nr >= 0 && nr < r && nc >= 0 && nc < c && !visited.contains(alphabets[nr][nc])) {
+                visited.add(alphabets[nr][nc]);
+                dfs(nr, nc, visited, depth + 1);
+                visited.remove(alphabets[nr][nc]);
+            }
+        }
     }
 }
