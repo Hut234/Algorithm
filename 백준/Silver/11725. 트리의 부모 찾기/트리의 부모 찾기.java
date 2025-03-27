755667e67df3fe5b7c165c4288a8f8ce0eccce85
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-03-27 18:57:47 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 472 ms, Memory: 72144 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static boolean[] visited;
+    static List<Integer>[] graph;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        visited = new boolean[N + 1];
+        graph = new List[N + 1];
+        result = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph[start].add(end);
+            graph[end].add(start);
+        }
+
+        dfs(1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(result[i]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static void dfs(int parent) {
+        visited[parent] = true;
+
+        for (Integer next : graph[parent]) {
+            if (!visited[next]) {
+                result[next] = parent;
+                dfs(next);
+            }
+        }
+    }
+}
