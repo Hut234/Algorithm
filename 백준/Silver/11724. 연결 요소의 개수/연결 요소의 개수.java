6ab12a14919d27504eca99207f7ce9c859b9d8e1
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-03-31 22:28:18 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 516 ms, Memory: 145284 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,38 +1,65 @@
-import java.io.*;
-class Main {
-	private static int N, M, CNT;
-	private static boolean[] VISITED;
-	private static boolean[][] INFO;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static int result;
+	static List<Integer>[] graph;
+	static boolean[] visited;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		String[] NM = br.readLine().split(" ");
 		N = Integer.parseInt(NM[0]);
 		M = Integer.parseInt(NM[1]);
-		VISITED = new boolean[N+1];
-		INFO = new boolean[N+1][N+1];
-		int s, e;
-		for(int i=0; i<M; i++) {
-			String[] info = br.readLine().split(" ");
-			s = Integer.parseInt(info[0]);
-			e = Integer.parseInt(info[1]);
-			INFO[s][e] = true;
-			INFO[e][s] = true;
+		graph = new List[N + 1];
+		visited = new boolean[N + 1];
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
 		}
-		
-		for(int i=1; i<=N; i++) {
-			if(!VISITED[i]) {
-				CNT++;
-				recur(i);
-			}
+
+		StringTokenizer st;
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+
+			graph[start].add(end);
+			graph[end].add(start);
 		}
-		System.out.print(CNT);
+
+		for (int i = 1; i <= N; i++) {
+			if (visited[i]) continue;
+			bfs(i);
+		}
+
+		System.out.print(result);
 	}
-	
-	private static void recur(int idx) {
-		if(VISITED[idx]) return;
-		VISITED[idx] = true;
-		for(int i=1; i<=N; i++) {
-			if(INFO[idx][i]) recur(i);
+
+	static void bfs(int start) {
+		result++;
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(start);
+		visited[start] = true;
+
+
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+
+			for (int next : graph[cur]) {
+				if (visited[next]) continue;
+				queue.offer(next);
+				visited[next] = true;
+			}
 		}
 	}
 }
