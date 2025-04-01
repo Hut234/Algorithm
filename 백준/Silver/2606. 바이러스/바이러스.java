fcfd458d669a9057fa8d754ad5ff3be63e5b4b3f
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-04-01 18:11:33 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 11712 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,42 +1,57 @@
-import java.io.*;
-import java.util.*;
-class Main {
-	private static int v, e, result;
-	private static boolean[] visited;
-	private static List<Integer>[] graph;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		v = Integer.parseInt(br.readLine());
-		e = Integer.parseInt(br.readLine());
-		visited = new boolean[v+1];
-		graph = new ArrayList[v+1];
-		for(int i=1; i<=v; i++) graph[i] = new ArrayList<>();
-		
-		int pc1, pc2;
-		for(int i=0; i<e; i++) {
-			String[] info = br.readLine().split(" ");
-			pc1 = Integer.parseInt(info[0]);
-			pc2 = Integer.parseInt(info[1]);
-			graph[pc1].add(pc2);
-			graph[pc2].add(pc1);
-			
-		}
-		dfs(1);
-		System.out.print(result);
-		
-	}
-	private static void dfs(int idx) {
-		visited[idx] = true;
-		Iterator<Integer> iter = graph[idx].iterator();
-		int tmp;
-		while(iter.hasNext()) {
-			tmp = iter.next();
-			if(!visited[tmp]) {
-				result++;
-				dfs(tmp);
-			}
-		}
-	}
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+public class Main {
+
+    static int n;
+    static int e;
+    static int result;
+    static boolean[][] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        e = Integer.parseInt(br.readLine());
+
+        graph = new boolean[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        StringTokenizer st;
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph[start][end] = true;
+            graph[end][start] = true;
+        }
+
+        bfs(1);
+        System.out.print(result);
+    }
+
+    static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+
+
+            for (int next = 1; next <= n; next++) {
+                if (graph[cur][next] && !visited[next]) {
+                    result++;
+                    queue.offer(next);
+                    visited[next] = true;
+                }
+            }
+        }
+    }
+}
