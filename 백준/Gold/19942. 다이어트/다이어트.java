a5712f70618a800e874c0ed6096d1ce6f707001c
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-04-03 14:38:25 +0900

    [Gold IV] Title: 다이어트, Time: 72 ms, Memory: 12980 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.java
+++ 백준/Gold/19942. 다이어트/다이어트.java
@@ -1,72 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  static int N, result = Integer.MAX_VALUE;
-  static int[] protein, fat, calorie, vitamin, price;
-  static boolean[] visited;
-  static StringBuilder sResult;
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    N = Integer.parseInt(br.readLine());
+    static StringBuilder sb;
+    static int n;
+    static int minPrice = Integer.MAX_VALUE;
+    static int[] standards = new int[4];
+    static Ingredient[] ingredients;
+    static boolean[] visited;
 
-    visited = new boolean[N+1];
-    protein = new int[N+1];
-    fat = new int[N+1];
-    calorie = new int[N+1];
-    vitamin = new int[N+1];
-    price = new int[N+1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    protein[0] = Integer.parseInt(st.nextToken());
-    fat[0] = Integer.parseInt(st.nextToken());
-    calorie[0] = Integer.parseInt(st.nextToken());
-    vitamin[0] = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
+        ingredients = new Ingredient[n + 1];
+        visited = new boolean[n + 1];
 
-    for (int i = 1; i <= N; i++) {
-      st = new StringTokenizer(br.readLine());
-      protein[i] = Integer.parseInt(st.nextToken());
-      fat[i] = Integer.parseInt(st.nextToken());
-      calorie[i] = Integer.parseInt(st.nextToken());
-      vitamin[i] = Integer.parseInt(st.nextToken());
-      price[i] = Integer.parseInt(st.nextToken());
-    }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        standards[0] = Integer.parseInt(st.nextToken());
+        standards[1] = Integer.parseInt(st.nextToken());
+        standards[2] = Integer.parseInt(st.nextToken());
+        standards[3] = Integer.parseInt(st.nextToken());
 
-    recur(1, 0, 0, 0, 0, 0);
-    if (result == Integer.MAX_VALUE) {
-      System.out.print(-1);
-    } else {
-      System.out.println(result);
-      System.out.print(sResult);
-    }
-  }
-
-  private static void recur(int idx, int p, int f, int c, int v, int pc) {
-
-    if (pc > result) return;
-
-    if (protein[0] <= p && fat[0] <= f && calorie[0] <= c && vitamin[0] <= v) {
-      if (result > pc) {
-        result = pc;
-        sResult = new StringBuilder();
-        for (int i = 1; i < N+1; i++) {
-          if(visited[i]) {
-            sResult.append(i).append(" ");
-          }
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            ingredients[i] = new Ingredient(st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken());
         }
-      }
+
+        dfs(1, 0, 0, 0, 0, 0);
+        System.out.print(minPrice == Integer.MAX_VALUE ? -1 : sb);
     }
 
-    if (idx == N+1) return;
+    static void dfs(int depth, int protein, int fat, int carbohydrate, int vitamin, int price) {
+        if (price >= minPrice) return;
 
-    visited[idx] = true;
-    recur(idx+1, p+protein[idx], f+fat[idx], c+calorie[idx], v+vitamin[idx], pc+price[idx]);
-    visited[idx] = false;
-    recur(idx+1, p, f, c, v, pc);
-  }
+        if (standards[0] <= protein && standards[1] <= fat && standards[2] <= carbohydrate && standards[3] <= vitamin) {
+            minPrice = price;
+            sb = new StringBuilder();
+            sb.append(price).append("\n");
+            for (int i = 1; i <= n; i++) {
+                if (visited[i]) {
+                    sb.append(i).append(" ");
+                }
+            }
+            return;
+        }
+
+        if (depth > n) return;
+
+
+        visited[depth] = true;
+        dfs(depth + 1, protein + ingredients[depth].protein, fat + ingredients[depth].fat,
+                carbohydrate + ingredients[depth].carbohydrate, vitamin + ingredients[depth].vitamin,
+                price + ingredients[depth].price);
+        visited[depth] = false;
+        dfs(depth + 1, protein, fat, carbohydrate, vitamin, price);
+    }
+
+    static class Ingredient {
+        int protein;
+        int fat;
+        int carbohydrate;
+        int vitamin;
+        int price;
+        Ingredient(String protein, String fat, String carbohydrate, String vitamin, String price) {
+            this.protein = Integer.parseInt(protein);
+            this.fat = Integer.parseInt(fat);
+            this.carbohydrate = Integer.parseInt(carbohydrate);
+            this.vitamin = Integer.parseInt(vitamin);
+            this.price = Integer.parseInt(price);
+        }
+    }
 }
