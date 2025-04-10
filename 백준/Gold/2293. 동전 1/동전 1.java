968f2ba58dd00147d63097c417d80625c119effb
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-04-10 22:25:25 +0900

    [Gold IV] Title: 동전 1, Time: 72 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -3,27 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] nk = br.readLine().split(" ");
+		int n = Integer.parseInt(nk[0]);
+		int k = Integer.parseInt(nk[1]);
 
-        String[] nk = br.readLine().split(" ");
-        int n = Integer.parseInt(nk[0]);
-        int k = Integer.parseInt(nk[1]);
 
-        int[] dp = new int[k + 1];
-        int[] coins = new int[n];
-        for (int i = 0; i < n; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-        }
+		int[] coins = new int[n];
+		for (int i = 0; i < n; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
 
-        dp[0] = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0, plus = coins[i]; plus <= k; j ++, plus++) {
-                dp[plus] += dp[j];
-            }
-        }
+		int[] dp = new int[k + 1];
+		dp[0] = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= k; j++) {
+				if (coins[i] + j > k) continue;
+				dp[coins[i] + j] += dp[j];
+			}
+		}
 
-        System.out.print(dp[k]);
-    }
+		System.out.print(dp[k]);
+	}
 }
