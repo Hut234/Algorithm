d0a1651389d4de2d7756926c82e5c4267891deeb
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-04-13 20:33:16 +0900

    [Gold IV] Title: N-Queen, Time: 2644 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,46 +1,45 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main {
-    
-    static int N;
-    static int result;
-    static int[] columns;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
-        columns = new int[N + 1];
-        
-        recur(1);
-        System.out.print(result);    
-    }
-    
-    static void recur(int row) {
-        if (row == N + 1) result++;
-        else {
-             for (int column = 1; column <= N; column++) {
-                 boolean isPossible = true;
-                 
-                 for (int row2 = 1; row2 < row; row2++) {
-                     int column2 = columns[row2];
-                     if (isAttackable(row, column, row2, column2)) {
-                         isPossible = false;
-                         break;
-                     }
-                 }
-                 
-                 if (isPossible) {
-                     columns[row] = column;
-                     recur(row + 1);
-                     columns[row] = 0;
-                 }
-                 
-             }
-        }
-    }
-    
-    static boolean isAttackable(int row, int column, int row2, int column2) {
-        return column == column2 || row2 - column2 == row - column || row2 + column2 == row + column;
-    }
+public class Main {
+
+	static int result;
+	static int N;
+	static boolean[] columns;
+	static boolean[] left2Right;
+	static boolean[] right2Left;
+
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		columns = new boolean[N + 1];
+		left2Right = new boolean[N * 2 + 1];
+		right2Left = new boolean[N * 2 + 1];
+
+		recur(1);
+		System.out.print(result);
+	}
+
+	static void recur(int row) {
+		if (row == N + 1) {
+			result++;
+			return;
+		}
+
+		for (int column = 1; column <= N; column++) {
+			if (columns[column] || left2Right[row - column + N] || right2Left[row + column]) continue;
+
+			columns[column] = true;
+			left2Right[row - column + N] = true;
+			right2Left[row + column] = true;
+
+			recur(row + 1);
+
+			columns[column] = false;
+			left2Right[row - column + N] = false;
+			right2Left[row + column] = false;
+		}
+	}
 }
