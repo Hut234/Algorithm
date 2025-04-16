775996cfbd2da6a00b2536647ab32013cac4fb23
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-04-16 22:54:50 +0900

    [level 0] Title: ad 제거하기, Time: 0.96 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public List<String> solution(String[] strArr) {
+        List<String> answer = new ArrayList<>();
+        
+        for (String str : strArr) {
+            if (str.contains("ad")) continue;
+            answer.add(str);
+        }
+        return answer;
+    }
+}
