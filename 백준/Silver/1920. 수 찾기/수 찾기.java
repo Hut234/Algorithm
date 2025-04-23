c0be42de3607a7b10d99df788df792f4625db747
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-04-23 16:46:40 +0900

    [Silver IV] Title: 수 찾기, Time: 508 ms, Memory: 53616 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,37 +1,51 @@
-import java.io.*;
-import java.util.*;
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String[] stdStr = br.readLine().split(" ");
-		int M = Integer.parseInt(br.readLine());
-		String[] inputStr = br.readLine().split(" ");
-		int[] std = new int[N];
-		for(int i=0; i<N; i++) {
-			std[i] = Integer.parseInt(stdStr[i]);
-		}
-		Arrays.sort(std);
-		int val, start, end, mid, result;
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<M; i++) {
-			val = Integer.parseInt(inputStr[i]);
-			start = 0;
-			end = N-1;
-			result = 0;
-			for(;!(start>end);) {
-				mid = (start+end) /2;
-				if(std[mid]>val) {
-					end = mid-1;
-				} else if(std[mid]<val) {
-					start = mid+1;
-				} else {
-					result = 1;
-					break;
-				}
-			}
-			sb.append(result).append("\n");
-		}
-		System.out.println(sb.toString());
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        int[] B = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+        for (int i = 0; i < M; i++) {
+            int X = B[i];
+            String result = "0";
+            int start = 0, end = N - 1;
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                int cur = A[mid];
+                if (cur > X) {
+                    end = mid - 1;
+                } else if (cur == X) {
+                    result = "1";
+                    break;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb);
+    }
 }
