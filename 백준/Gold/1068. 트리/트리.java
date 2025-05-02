fb6becbb590f5ba0fdc9b0eb3860a61d6d7436d2
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-05-02 16:12:52 +0900

    [Gold V] Title: 트리, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Main {
+
+	static int n;
+	static boolean[] visited;
+	static int result;
+	static int root;
+	static int remove;
+	static List<Integer>[] graph;
+
+	public static void main(String[] args) throws IOException {
+		getInput();
+		solve();
+	}
+
+	static void getInput() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		visited = new boolean[n];
+		graph = new List[n];
+
+		for (int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<>();
+		}
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		remove = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			int next = Integer.parseInt(st.nextToken());
+			if (next == -1) {
+				root = i;
+				continue;
+			}
+			if (next == remove || i == remove) continue;
+			graph[i].add(next);
+			graph[next].add(i);
+		}
+	}
+
+	static void solve() {
+		dfs(root);
+		print();
+	}
+
+	static void dfs(int parent) {
+		if (parent == remove) return;
+		visited[parent] = true;
+
+		for (int child : graph[parent]) {
+			if (visited[child]) continue;
+			dfs(child);
+			visited[child] = false;
+		}
+
+		if (isLeaf(parent)) result++;
+	}
+
+	static boolean isLeaf(int node) {
+		boolean flag = true;
+		for (int next : graph[node]) {
+			if (!visited[next]) {
+				flag = false;
+				break;
+			}
+		}
+
+		return flag;
+	}
+
+	static void print() {
+		System.out.print(result);
+	}
+}
