ddef59bee16f1cacd586318bca56705f88306120
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-06-12 12:09:38 +0900

    [Silver I] Title: 경로 찾기, Time: 280 ms, Memory: 21372 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static List<Integer>[] graph;
+    static boolean[][] visited;
+    static String[][] maps;
+    static int[][] result;
+
+    public static void main(String[] args) {
+        getInput();
+        solve();
+    }
+
+    static void solve() {
+        for (int row = 0; row < N; row++) {
+            for (Integer col : graph[row]) {
+                dfs(row, col);
+            }
+        }
+
+        for (int[] values : result) {
+            for (int value : values) {
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void dfs(int root, int next) {
+        result[root][next] = 1;
+        visited[root][next] = true;
+        for (int i = 0; i < N; i++) {
+            if ("0".equals(maps[next][i]) || visited[root][i]) continue;
+            dfs(root, i);
+        }
+    }
+
+
+    static void getInput() {
+        N = sc.nextInt();
+        graph = new List[N];
+        visited = new boolean[N][N];
+        maps = new String[N][N];
+        result = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                String value = sc.next();
+                maps[i][j] = value;
+                if ("1".equals(value)) graph[i].add(j);
+            }
+        }
+    }
+}
