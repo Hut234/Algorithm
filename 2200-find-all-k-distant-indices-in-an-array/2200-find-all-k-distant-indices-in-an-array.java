2548adfac7f64bf1119d64db78d79712e1ea5737
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-06-25 13:33:43 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.java
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.java
@@ -2,21 +2,25 @@ import java.util.List;
 import java.util.ArrayList;
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        List<Integer> keyIndices = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == key) keyIndices.add(i);
-        }
-
         List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int index : keyIndices) {
-                if (Math.abs(i - index) <= k) {
-                    answer.add(i);
-                    break;
+        int checkedIndex = 0;
+        for (int i = 0; i < nums.length;) {
+            if (nums[i] == key) {
+                boolean flag = true;
+                int j = Integer.max(checkedIndex, i - k);
+                for (; j < Integer.min(i + k + 1, nums.length); j++) {
+                    if (i < j && nums[j] == key) {
+                        i = j;
+                        checkedIndex = j;
+                        flag = false;
+                        break;
+                    }
+                    answer.add(j);
                 }
-            }
-        }
+                if (flag) i = j + 1;
+            } else i++;
 
+        }
         return answer;
     }
 }
