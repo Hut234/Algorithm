cb784700ea42346a97fe2666f5d86fac87a676b0
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-10-08 18:58:10 +0900

    [Gold IV] Title: 리모컨, Time: 344 ms, Memory: 101940 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,58 +1,38 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-	static int N;
-	static int M;
-	static int length;
-	static int result = 500000;
-	static List<Integer> availableButtons = new ArrayList<>();
-
 	public static void main(String[] args) {
-		getInput();
-		solve();
-	}
 
-	private static void solve() {
-		dfs(0, 0);
-		int answer = Integer.min(Math.abs(N - 100), result);
-		System.out.print(answer);
-	}
-
-	private static void dfs(int depth, int currentChannel) {
-		if (depth > length + 1) return;
-
-		if (depth != 0) {
-			result = Integer.min(result, Math.abs(N - currentChannel) + depth);
-		}
-
-		int len = availableButtons.size();
-		for (int i = 0; i < len; i++) {
-			currentChannel = currentChannel * 10 + availableButtons.get(i);
-			dfs(depth + 1, currentChannel);
-			currentChannel /= 10;
-		}
-	}
-
-	private static void getInput() {
 		Scanner sc = new Scanner(System.in);
-		String n = sc.nextLine();
-		N = Integer.parseInt(n);
-		length = n.length();
-		M = sc.nextInt();
-		int[] brokenButtons = new int[M];
-		for (int i = 0; i < M; i++) {
-			brokenButtons[i] = sc.nextInt();
-		}
-		Arrays.sort(brokenButtons);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
 
-		for (int i = 0; i <= 9; i++) {
-			if (Arrays.binarySearch(brokenButtons, i) < 0) {
-				availableButtons.add(i);
-			}
+		boolean[] unavailable = new boolean[10];
+		for (int i = 0; i < M; i++) {
+			unavailable[sc.nextInt()] = true;
 		}
+
+		int answer = Math.abs(N - 100);
+		for (int i = 0; i < 999999; i++) {
+			if (answer == 0) break;
+
+			String currentChannel = String.valueOf(i);
+
+			boolean possible = true;
+			for (char c : currentChannel.toCharArray()) {
+				if (unavailable[c - '0']) {
+					possible = false;
+					break;
+				}
+			}
+
+			if (!possible) continue;
+
+			int count = currentChannel.length() + Math.abs(N - i);
+			answer = Math.min(answer, count);
+		}
+
+		System.out.print(answer);
 	}
 }
