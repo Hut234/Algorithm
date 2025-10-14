1bce7613373d6a382e3a473811c5548fb8d85c18
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-10-14 17:59:16 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int N = new Scanner(System.in).nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            print(sb, N, i);
+        }
+ 
+        for (int i = N - 1; i > 0; i--) {
+            print(sb, N, i);
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void print(StringBuilder sb, int N, int i) {
+        int spaces = N - i;
+        int stars = i;
+
+        sb.append(" ".repeat(spaces));
+        sb.append("*".repeat(stars));
+        sb.append("\n");
+    }
+}
