3fe218436c2dae8b84b9290d351edaffdae7821a
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-10-16 23:29:11 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 13276 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,30 +1,27 @@
-import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) throws IOException {
-
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-
-		int[] scores = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			scores[i] = sc.nextInt();
-		}
-
-		int[][] dp = new int[N + 1][N + 1];
-
-		dp[1][1] = scores[1];
-		if (N >= 2) {
-			dp[2][0] = scores[2];
-			dp[2][1] = scores[1] + scores[2];
-		}
-		for (int i = 3; i <= N; i++) {
-			dp[i][0] = Integer.max(dp[i - 2][0], dp[i - 2][1]) + scores[i];
-			dp[i][1] = dp[i - 1][0] + scores[i];
-		}
-
-		System.out.print(Integer.max(dp[N][0], dp[N][1]));
-	}
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int[] stairs = new int[N + 1];
+        for (int i = 1; i <= N; i++) stairs[i] = sc.nextInt();
+        
+        int[][] dp = new int[N + 1][3];
+        dp[1][1] = stairs[1];
+        
+        if (N > 1) {
+            dp[2][1] = stairs[1] + stairs[2];
+            dp[2][2] = stairs[2];
+        }
+        
+        
+        for (int i = 3; i <= N; i++) {
+            dp[i][1] = dp[i - 1][2] + stairs[i];
+            dp[i][2] = Integer.max(dp[i - 2][1], dp[i - 2][2]) + stairs[i];
+        }
+        
+        System.out.print(Integer.max(dp[N][1], dp[N][2]));
+    }
 }
