0a37b290a3e919627dad1db721a3010346212cfd
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-10-18 03:14:39 +0900

    [Silver II] Title: 촌수계산, Time: 68 ms, Memory: 11712 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,42 +1,67 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 class Main {
-	private static int[] visited;
-	private static List<Integer>[] graph;
-	private static int a, b, result;
+
+	static int n;
+	static int m;
+	static int start;
+	static int end;
+	static boolean[] visited;
+	static List<Integer>[] relations;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		visited = new int[N+1];
-		Arrays.fill(visited, -1);
-		graph = new ArrayList[N+1];
-		for(int i=1; i<=N; i++) graph[i] = new ArrayList<>();
-		
-		String[] ab = br.readLine().split(" ");
-		a = Integer.parseInt(ab[0]);
-		b = Integer.parseInt(ab[1]);
-		
-		int M = Integer.parseInt(br.readLine());
-		int x, y;
-		for(int i=0; i<M; i++) {
-			String[] xy = br.readLine().split(" ");
-			x = Integer.parseInt(xy[0]);
-			y = Integer.parseInt(xy[1]);
-			graph[x].add(y);
-			graph[y].add(x);
-		}
-		
-		visited[a] = 0;
-		dfs(a);
-		System.out.println(visited[b]);
+		getInput();
+		solve();
 	}
-	
-	private static void dfs(int idx) {
-		for(Integer i : graph[idx]) {
-			if(visited[i] == -1) {
-				visited[i] = visited[idx] +1;
-				dfs(i);
+
+	private static void solve() {
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[]{start, 0});
+		visited[start] = true;
+
+		int answer = -1;
+		while (!queue.isEmpty()) {
+			int[] current = queue.poll();
+			if (current[0] == end) {
+				answer = current[1];
+				break;
 			}
+			for (int next : relations[current[0]]) {
+				if (visited[next]) continue;
+				queue.offer(new int[]{next, current[1] + 1});
+				visited[next] = true;
+			}
+		}
+		System.out.print(answer);
+	}
+
+
+	private static void getInput() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(br.readLine());
+		visited = new boolean[n + 1];
+
+		relations = new List[n + 1];
+		for (int i = 1; i <= n; i++) {
+			relations[i] = new ArrayList<>();
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int parent = Integer.parseInt(st.nextToken());
+			int child = Integer.parseInt(st.nextToken());
+			relations[parent].add(child);
+			relations[child].add(parent);
 		}
 	}
 }
