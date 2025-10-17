c51b1d11d62c4720273de62f763a23ca769f717a
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-10-18 04:32:49 +0900

    [Gold IV] Title: 공유기 설치, Time: 296 ms, Memory: 38504 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,49 +1,45 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 class Main {
-    static int N;
-    static int C;
-    static int[] houses;
-    public static void main(String[] args) {
-        getInput();
-        solve();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void getInput() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        C = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
 
-        houses = new int[N];
-        for (int i = 0; i < N; i++) houses[i] = sc.nextInt();
-        Arrays.sort(houses);
-        sc.close();
-    }
+		int[] houses = new int[N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			houses[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(houses);
 
-    public static void solve() {
-        int start = 1, end = 1000000000;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (determination(mid)) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        System.out.print(end);
-    }
+		int start = 0, end = 1_000_000_000;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (determine(houses, mid, C)) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
 
-    public static boolean determination(int target) {
-        int count = 1;
-        int prev = houses[0];
-        for (int i = 1; i < N; i++) {
-            if (houses[i] - prev >= target) {
-                prev = houses[i];
-                count++;
-            }
-        }
+		System.out.print(end);
+	}
 
-        return count >= C;
-    }
+	private static boolean determine(int[] houses, int distance, int routerCount) {
+		int count = 1;
+		int prev = houses[0];
+		int len = houses.length;
+		for (int i = 1; i < len; i++) {
+			if (houses[i] - prev >= distance) {
+				prev = houses[i];
+				count++;
+			}
+		}
+
+		return routerCount <= count;
+	}
 }
