7b8199b5a27100c7ccb1a6ce08e746f9064b9338
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-10-23 23:32:13 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string.substring(0, s));
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(my_string.substring(s, e + 1));
+        sb2.reverse();
+        sb.append(sb2.toString());
+        sb.append(my_string.substring(e + 1));
+        return sb.toString();
+    }
+}
