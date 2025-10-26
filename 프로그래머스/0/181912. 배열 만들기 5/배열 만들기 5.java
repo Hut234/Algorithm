48873c2ce528ec673904798bd862868de9599473
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-10-26 23:03:21 +0900

    [level 0] Title: 배열 만들기 5, Time: 6.57 ms, Memory: 92.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (String intStr : intStrs) {
+            int value = Integer.parseInt(intStr.substring(s, s + l));
+            if (value > k) answer.add(value);
+        }
+        
+        return answer;
+    }
+}
