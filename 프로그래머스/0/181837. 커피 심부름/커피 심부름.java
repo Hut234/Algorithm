138b953b5ebd9778962695c146b78c454c190845
Hut234 <135043156+Hut234@users.noreply.github.com>
2025-11-14 22:46:52 +0900

    [level 0] Title: 커피 심부름, Time: 0.19 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for (String s : order) {
+            if (s.equals("iceamericano") || s. equals("americanoice") || s.equals("hotamericano") || s.equals("americanohot") || s.equals("americano") || s.equals("anything")) {
+                answer += 4500;
+            } else {
+                answer += 5000;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
